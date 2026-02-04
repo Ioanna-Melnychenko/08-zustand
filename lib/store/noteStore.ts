948f60f43e1399d84c4efe6dfc016bfa0c3c39bc
Inna Melnychenko <dovgopolaii@gmail.com>
2026-02-04 14:09:49 +0100

    comment

--- lib/store/noteStore.ts
+++ lib/store/noteStore.ts
@@ -1,22 +1,22 @@
-// app/lib/stores/noteStore.ts
+// // app/lib/stores/noteStore.ts
 
-import { create } from 'zustand';
-import { NewNoteData } from '../api';
+// import { create } from 'zustand';
+// import { NewNoteData } from '../api';
 
-type NoteDraftStore = {
-  draft: NewNoteData;
-  setDraft: (note: NewNoteData) => void;
-  clearDraft: () => void;
-};
+// type NoteDraftStore = {
+//   draft: NewNoteData;
+//   setDraft: (note: NewNoteData) => void;
+//   clearDraft: () => void;
+// };
 
-const initialDraft: NewNoteData = {
-  title: '',
-  content: '',
-  categoryId: '',
-};
+// const initialDraft: NewNoteData = {
+//   title: '',
+//   content: '',
+//   categoryId: '',
+// };
 
-export const useNoteDraftStore = create<NoteDraftStore>()((set) => ({
-  draft: initialDraft,
-  setDraft: (note) => set(() => ({ draft: note })),
-  clearDraft: () => set(() => ({ draft: initialDraft })),
-}));
+// export const useNoteDraftStore = create<NoteDraftStore>()((set) => ({
+//   draft: initialDraft,
+//   setDraft: (note) => set(() => ({ draft: note })),
+//   clearDraft: () => set(() => ({ draft: initialDraft })),
+// }));
